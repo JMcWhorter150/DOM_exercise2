a991d601a9b84eddcf6efc7196a077a4c0d1a42b
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-06 15:20:30 -0500

    Adds the ability to display anchors using js

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,26 @@ const addresses = [
     "https://duckduckgo.com"
 ];
 
+const linkContainer = document.querySelector(".js-link-container");
+
+function makeAnchorElement(link) {
+    // console.log(link);
+    let anchorElement = document.createElement("a");
+    anchorElement.href = link;
+    anchorElement.textContent = link;
+    // console.log(anchorElement);
+    return anchorElement;
+}
+
+function displayElement(el) {
+    // console.log('display element');
+    linkContainer.appendChild(el);
+}
+
+function displayAnchor(link) {
+    // console.log('display anchor');
+    let aEl = makeAnchorElement(link);
+    displayElement(aEl);
+}
+
+addresses.map(displayAnchor);
